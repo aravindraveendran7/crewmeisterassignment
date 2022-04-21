9c59c0900ff12a73124de0c959fe11dafe854d37
Aravind Raveendran <aravindraveendran55@gmail.com>
2022-04-21 10:26:04 +0530

    adding config.properties resouces and classes

--- src/main/java/configs/ConfigFactory.java
+++ src/main/java/configs/ConfigFactory.java
@@ -5,7 +5,6 @@ import org.aeonbits.owner.ConfigCache;
 public final class ConfigFactory {
 
     private ConfigFactory(){}
-
     public static FrameworkConfigs getConfig(){
         return ConfigCache.getOrCreate(FrameworkConfigs.class);
     }
--- src/main/java/configs/FrameworkConfigs.java
+++ src/main/java/configs/FrameworkConfigs.java
@@ -4,7 +4,6 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources(value = "file:${user.dir}/src/test/resources/config/config.properties")
 public interface FrameworkConfigs extends Config {
-
     String timeout();
     String url();
     String browser();
